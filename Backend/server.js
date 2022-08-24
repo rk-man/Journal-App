b6be900531df3b9c0c9e883c680d7f5a8dda3d6d
rk_man <naveenbadi2003@gmail.com>
2022-08-24 22:46:35 +0530

    one more change

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,8 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
 const PORT = process.env.PORT || 5000;
+const express = require("express");
+const path = require("path");
 
 let DB_CON_STRING = process.env.DB_CON_STRING.replace(
     "<password>",
@@ -18,6 +20,18 @@ mongoose
     });
 
 const app = require("./app");
+
+///serve static assets...specifying the folder in which heroku wants to search
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+    //serve static file
+    app.get("*", (req, res) => {
+        res.sendFile(
+            path.resolve(__dirname, "../", "frontend", "build", "index.html")
+        );
+    });
+}
 
 app.listen(PORT, () => {
     console.log(`server is listening at port ${PORT}`);
